5150ab6f73ed13623655c5f178d760a540acb054
ElifRuveyha <ruveyhaozmen@gmail.com>
2022-12-26 10:22:45 +0100

    Add create endpoint to create short url with alias

--- app/main.py
+++ app/main.py
@@ -1,7 +1,41 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import requests
+
+from config import TOKEN
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+@app.route("/create", methods=["POST"])
+def create_short_url():
+    # Get the destination url from the request body
+    url = request.json.get("destintation")
+    # Get the alias from the request body
+    alias = request.json.get("slashtag")
+
+    # Create the short url
+    short_url = create_short_url(url, alias)
+
+    # Return the short url
+    return jsonify({"short_url": short_url})
+
+
+def create_short_url(url, alias):
+    # Set the base url
+    BASE_URL = "https://api.rebrandly.com/v1/links"
+    # Set the header
+    header = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "apikey": TOKEN
+    }
+    # Set the body
+    body = {
+        "destination": url,
+        "slashtag": alias,
+    }
+    # Make the request
+    response = requests.post(BASE_URL, json=body, headers=header)
+    # Return the short url
+    json = response.json()
+    return json["shortUrl"]
